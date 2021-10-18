e92ac9510dfd498e265dda2e710eed2fedf80f2f
fuka-innovations <fuad.kalhori@gmail.com>
2021-10-18 10:01:31 +0300

    3.4: puhelinluettelon backend step4

--- app.js
+++ app.js
@@ -4,20 +4,32 @@ app.use( express.json() );
 
 // Get all persons
 app.get( '/api/persons', ( req, res ) => {
-  res.json( notes );
+  res.json( persons );
 } );
 
 // Get person by id
 app.get( '/api/persons/:id', ( req, res ) => {
   const personId = Number(req.params.id)
-  const personFound = notes.find(person => person.id === personId );
+  const personFound = persons.find(person => person.id === personId );
   personFound ? res.json(personFound) : res.status(400).end()
+} );
+
+// Delete person by id
+app.delete( '/api/persons/:id', ( req, res ) => {
+  const personId = Number(Math.trunc(req.params.id))
+  personId ?  personFound = persons.find(person => person.id === personId ) : res.status(400).json({message: 'Id should be intiger'})
+  if (personFound) {
+    persons.filter(person => person.id !== personId)
+    res.status(204).end()
+  } else {
+    res.status(400).json({message: `Person with id ${personId} not found`})
+  }
 } );
 
 // Get total amount of phone book entries
 app.get( '/info', ( req, res ) => {
   const timeStamp = Date();
-  const personsCount = notes.length;
+  const personsCount = persons.length;
   res.send(`<p>Phone book has info of ${personsCount} people</p><p>${timeStamp}</p>`)
 } );
 
@@ -27,23 +39,25 @@ app.listen( PORT, () => {
   console.log( `Server running on port ${ PORT }` );
 } );
 
-let notes = [
+let persons = [
   {
     id: 1,
-    content: 'HTML is easy',
-    date: '2020-01-10T17:30:31.098Z',
-    important: true,
+    name: 'Arto Hellas',
+    number: '040-123456'
   },
   {
     id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2020-01-10T18:39:34.091Z',
-    important: false,
+    name: 'Ada Lovelace',
+    number: '39-324234324'
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2020-01-10T19:20:14.298Z',
-    important: true,
+    name: 'Dan Sukunimi',
+    number: '045-3424324'
   },
+  {
+    id: 4,
+    name: 'Aary Monroes',
+    number: '044-23423423'
+  }
 ];
