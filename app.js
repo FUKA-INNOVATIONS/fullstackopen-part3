9373dc31ec2b0914c46a152fffca08f825b165bb
fuka-innovations <fuad.kalhori@gmail.com>
2021-10-18 08:35:38 +0300

    3.2: puhelinluettelon backend step2

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+const express = require( 'express' );
+const app = express();
+app.use( express.json() );
 
+app.get( '/api/persons', ( req, res ) => {
+  res.json( notes );
+} );
 
-app.get('/api/persons', (req, res) => {
-  res.json(notes)
-})
+app.get( '/api/persons/:id', ( req, res ) => {
+  let person;
+  res.json( notes );
+} );
 
-app.get('/api/persons/:id', (req, res) => {
-  let person
-  res.json(notes)
-})
+app.get( '/info', ( req, res ) => {
+  const timeStamp = Date();
+  const personsCount = notes.length;
+  res.send(`<p>Phone book has info of ${personsCount} people</p><p>${timeStamp}</p>`)
+} );
 
-
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
+const PORT = 3001;
+app.listen( PORT, () => {
+  console.log( `Server running on port ${ PORT }` );
+} );
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
+    content: 'HTML is easy',
+    date: '2020-01-10T17:30:31.098Z',
+    important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
+    content: 'Browser can execute only Javascript',
+    date: '2020-01-10T18:39:34.091Z',
+    important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2020-01-10T19:20:14.298Z',
+    important: true,
+  },
+];
